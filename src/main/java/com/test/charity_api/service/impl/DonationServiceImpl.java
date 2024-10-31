6259044527f078ddd3f6efc913812cd8444eadad
Minh Huy <huylekore.071103@gmail.com>
2024-10-31 09:56:58 +0700

    Hoàn thành task:Tìm kiếm quyên góp

--- src/main/java/com/test/charity_api/service/impl/DonationServiceImpl.java
+++ src/main/java/com/test/charity_api/service/impl/DonationServiceImpl.java
@@ -1,19 +1,23 @@
 package com.test.charity_api.service.impl;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import com.test.charity_api.dto.DonationDTO;
 import com.test.charity_api.dto.DonationResponse;
 import com.test.charity_api.entity.Donation;
 import com.test.charity_api.mapper.DonationMapper;
 import com.test.charity_api.repository.DonationRepository;
 import com.test.charity_api.service.DonationService;
-import java.text.ParseException;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
 @Service
 public class DonationServiceImpl implements DonationService {
@@ -24,7 +28,7 @@ public class DonationServiceImpl implements DonationService {
     @Override
     public DonationResponse getDonation(int pageNo, int pageSize, Long campaignId, String name) {
         Pageable pageable = PageRequest.of(pageNo, pageSize);
-        Page<Donation> result = null;
+        Page<Donation> result;
 
         String queryStr = name.trim().toLowerCase();
         if (queryStr.isEmpty()) {
@@ -34,7 +38,6 @@ public class DonationServiceImpl implements DonationService {
         } else {
             result = donationRepository.findByCampaignIdAndDonorName(campaignId, queryStr, pageable);
         }
-
         List<DonationDTO> content = result.getContent().stream()
                 .map(d -> DonationMapper.mapToDonationDto(d, false, true, true, false))
                 .collect(Collectors.toList());
@@ -49,6 +52,7 @@ public class DonationServiceImpl implements DonationService {
         return response;
     }
 
+
     @Override
     public void insert(DonationDTO d) throws ParseException {
         Donation entity = null;
@@ -58,6 +62,44 @@ public class DonationServiceImpl implements DonationService {
             throw ex;
         }
         donationRepository.save(entity);
+    }
+
+    // phần huyle
+    @Override
+    public DonationResponse searchDonations(int pageNo, int pageSize, String campaignName, String donorName, String startDate, String endDate) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Page<Donation> result;
+        Date start = null;
+        Date end = null;
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            if (startDate != null && !startDate.trim().isEmpty()) {
+                start = dateFormat.parse(startDate);
+            }
+            if (endDate != null && !endDate.trim().isEmpty()) {
+                end = dateFormat.parse(endDate);
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException("Đã nhập sai format. Hãy dùng yyyy-MM-dd.");
+        }
+
+        if (start != null && end != null && start.after(end)) {
+            throw new RuntimeException("Ngày bắt đầu không thể lớn hơn ngày kết thúc.");
+        }
+
+        result = donationRepository.findByCampaignIdAndDonorNameAndDateRange(campaignName, donorName, start, end, pageable);
+        List<DonationDTO> content = result.getContent().stream()
+                .map(d -> DonationMapper.mapToDonationDto(d, false, true, true, false))
+                .collect(Collectors.toList());
+
+        DonationResponse response = new DonationResponse();
+        response.setContent(content);
+        response.setPageNo(pageNo + 1);
+        response.setPageSize(pageSize);
+        response.setTotalElements(result.getTotalElements());
+        response.setTotalPages(result.getTotalPages());
+        response.setLast(result.isLast());
+        return response;
     }
 
 }
