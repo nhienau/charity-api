f4bc07bdf8c51a950e15fdf5f6f7533ac4928b9a
Nhien <nhienau@gmail.com>
2024-12-04 19:50:04 +0700

    Fetch donor name by phone number, fix handle donation

--- src/main/java/com/test/charity_api/repository/DonorNameRepository.java
+++ src/main/java/com/test/charity_api/repository/DonorNameRepository.java
@@ -1,6 +1,8 @@
 package com.test.charity_api.repository;
 
 import com.test.charity_api.entity.DonorName;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,4 +11,14 @@ public interface DonorNameRepository extends JpaRepository<DonorName, Long> {
 
     @Query("SELECT dn FROM DonorName dn WHERE dn.donor.id = :donorId AND dn.name = :name")
     DonorName findByNameAndDonorId(@Param("name") String name, @Param("donorId") String donorId);
+
+    @Query("""
+           SELECT ELEMENT(d.donorName)
+           FROM Donor d
+           WHERE d.status = true AND d.phoneNumber LIKE :phoneNumber
+           ORDER BY ELEMENT(d.donorName).name ASC
+           """)
+    Page<DonorName> getDonorNamesByPhoneNumber(@Param("phoneNumber") String phoneNumber, Pageable pageable);
+
+    DonorName findById(int id);
 }
--- src/main/java/com/test/charity_api/util/HtmlUtil.java
+++ src/main/java/com/test/charity_api/util/HtmlUtil.java
@@ -6,12 +6,18 @@ import org.apache.commons.text.StringEscapeUtils;
 public class HtmlUtil {
 
     public static String escapeHTML(String str) {
+        if (str == null) {
+            return null;
+        }
         return str.codePoints().mapToObj(c -> c > 127 || "\"'<>&".indexOf(c) != -1
                 ? "&#" + c + ";" : new String(Character.toChars(c)))
                 .collect(Collectors.joining());
     }
 
     public static String unescapeHTML(String str) {
+        if (str == null) {
+            return null;
+        }
         return StringEscapeUtils.unescapeHtml4(str);
     }
 }
